e17adc4770e6643beab0a0d54e16ee120b4f2fea
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-12-15 18:42:04 -0800

    Update auto mapper config file

--- Hotel.Web_API/App_Start/AutoMapperConfiguration.cs
+++ Hotel.Web_API/App_Start/AutoMapperConfiguration.cs
@@ -11,20 +11,20 @@ namespace Hotel.Web_API.App_Start
             var config = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<Order, OrderDto>()
-                    .ForMember("OrderNumber", (IMemberConfigurationExpression<Order, OrderDto, object> opt) => opt.MapFrom(p => p.Id))
-                    .ForMember("RoomNumber", (IMemberConfigurationExpression<Order, OrderDto, object> opt) => opt.MapFrom(p => p.RoomId));
+                    .ForMember("OrderNumber", opt => opt.MapFrom(p => p.Id))
+                    .ForMember("RoomNumber", opt => opt.MapFrom(p => p.RoomId));
                 cfg.CreateMap<Room, RoomDto>()
-                    .ForMember("RoomNumber", (IMemberConfigurationExpression<Room, RoomDto, object> opt) => opt.MapFrom(p => p.Id));
+                    .ForMember("RoomNumber", opt => opt.MapFrom(p => p.Id));
                 cfg.CreateMap<RoomCategory, RoomCategoryDto>()
-                    .ForMember("CategoryNumber", (IMemberConfigurationExpression<RoomCategory, RoomCategoryDto, object> opt) => opt.MapFrom(p => p.Id));
+                    .ForMember("CategoryNumber", opt => opt.MapFrom(p => p.Id));
 
                 cfg.CreateMap<OrderDto, Order>()
-                    .ForMember("Id", (IMemberConfigurationExpression<OrderDto, Order, object> opt) => opt.MapFrom(p => p.OrderNumber))
-                    .ForMember("RoomId", (IMemberConfigurationExpression<OrderDto, Order, object> opt) => opt.MapFrom(p => p.RoomNumber));
+                    .ForMember("Id", opt => opt.MapFrom(p => p.OrderNumber))
+                    .ForMember("RoomId", opt => opt.MapFrom(p => p.RoomNumber));
                 cfg.CreateMap<RoomDto, Room>()
-                    .ForMember("Id", (IMemberConfigurationExpression<RoomDto, Room, object> opt) => opt.MapFrom(p => p.RoomNumber));
+                    .ForMember("Id", opt => opt.MapFrom(p => p.RoomNumber));
                 cfg.CreateMap<RoomCategoryDto, RoomCategory>()
-                     .ForMember("Id", (IMemberConfigurationExpression<RoomCategoryDto, RoomCategory, object> opt) => opt.MapFrom(p => p.CategoryNumber));
+                     .ForMember("Id", opt => opt.MapFrom(p => p.CategoryNumber));
                 
             });
 
